516b3dee16f16479c5816f492af0d9ae2010a6d1
kntsv <kuntsevych2020ki12@student.karazin.ua>
2024-06-22 11:20:17 +0300

    feat: add Animal abstract class

---
+++ src/main/java/org/example/entities/animals/abstraction/Animal.java
@@ -0,0 +1,9 @@
+package org.example.entities.animals.abstraction;
+
+import org.example.entities.interfaces.Eatable;
+import org.example.entities.interfaces.Movable;
+import org.example.entities.interfaces.Organism;
+
+public abstract class Animal implements Organism, Movable, Eatable {
+
+}
