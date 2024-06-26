1f34599ffe8dc9ce0ce181e3cc62834146d77d5a
kntsv <kuntsevych2020ki12@student.karazin.ua>
2024-06-26 20:18:54 +0300

    feat: implement code logic for classes Grass and Plant

--- src/main/java/org/example/entities/plants/Grass.java
+++ src/main/java/org/example/entities/plants/Grass.java
@@ -1,7 +1,30 @@
 package org.example.entities.plants;
 
-import org.example.entities.interfaces.Organism;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import lombok.experimental.SuperBuilder;
+import org.example.abstraction.annotations.Config;
+import org.example.abstraction.annotations.GameObjectEntity;
+import org.example.abstraction.interfaces.GameObject;
 
+@GameObjectEntity
+@Config(fileName = "")
+@NoArgsConstructor
+@SuperBuilder
+@EqualsAndHashCode(callSuper = true)
+@ToString(callSuper = true)
 public class Grass extends Plant {
 
+    @Override
+    public void play() {
+
+    }
+
+    @Override
+    public GameObject reproduce() {
+        return null;
+    }
 }
+
+
--- src/main/java/org/example/entities/plants/Plant.java
+++ src/main/java/org/example/entities/plants/Plant.java
@@ -1,7 +1,29 @@
 package org.example.entities.plants;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import lombok.experimental.SuperBuilder;
 import org.example.entities.interfaces.Organism;
+import org.example.entities.map.Cell;
 
+@NoArgsConstructor
+@SuperBuilder
+@Getter
+@EqualsAndHashCode
+@ToString
 public abstract class Plant implements Organism {
 
+    @JsonIgnore
+    private Cell cell;
+
+    private int maxAmount;
+
+    private String icon;
+    private boolean isAlive = true;
+    private int weight;
+    private int health;
+    private int age;
 }
