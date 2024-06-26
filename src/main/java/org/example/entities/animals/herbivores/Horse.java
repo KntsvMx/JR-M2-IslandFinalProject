80e83e3f7fede9ecd6ae990dfc11c54e7610b370
kntsv <kuntsevych2020ki12@student.karazin.ua>
2024-06-26 09:30:49 +0300

    feat: add limits and Horse class
    Limits help to decompose logic in abstract classes. Horse implements some code realization

---
+++ src/main/java/org/example/entities/animals/herbivores/Horse.java
@@ -0,0 +1,30 @@
+package org.example.entities.animals.herbivores;
+
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import lombok.experimental.SuperBuilder;
+import org.example.abstraction.annotations.Config;
+import org.example.abstraction.annotations.GameObjectEntity;
+import org.example.abstraction.interfaces.GameObject;
+import org.example.entities.animals.abstractions.Herbivore.Herbivore;
+
+@GameObjectEntity
+@Config(fileName = "model/yamlFormat/horst.yaml")
+@NoArgsConstructor
+@SuperBuilder
+@EqualsAndHashCode(callSuper = true)
+@ToString(callSuper = true)
+
+public class Horse extends Herbivore {
+
+    @Override
+    public GameObject reproduce() {
+        return Horse.builder()
+                .limits(getLimits())
+                .health(100)
+                .age(0)
+                .isAlive(true)
+                .build();
+    }
+}
---
+++ src/main/java/org/example/entities/limits/Limits.java
@@ -0,0 +1,11 @@
+package org.example.entities.limits;
+
+import lombok.Data;
+@Data
+public class Limits {
+    private int maxWeight;
+    private int maxAmount;
+    private int maxSpeed;
+    private int maxFood;
+    private int maxAge;
+}
