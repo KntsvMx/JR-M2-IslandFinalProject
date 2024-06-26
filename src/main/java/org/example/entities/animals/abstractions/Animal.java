419a1953377a629f1f29c52219e0c1cb24511234
kntsv <kuntsevych2020ki12@student.karazin.ua>
2024-06-26 09:27:08 +0300

    feat: code implements into the Animal class and extends it to Predator and Herbivore

--- src/main/java/org/example/entities/animals/abstractions/Animal.java
+++ src/main/java/org/example/entities/animals/abstractions/Animal.java
@@ -1,9 +1,51 @@
 package org.example.entities.animals.abstractions;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
 import org.example.entities.interfaces.Eatable;
 import org.example.entities.interfaces.Movable;
 import org.example.entities.interfaces.Organism;
+import org.example.entities.limits.Limits;
+import org.example.entities.map.Cell;
+
+@NoArgsConstructor
+@SuperBuilder
+@Getter
+@EqualsAndHashCode
+@ToString
 
 public abstract class Animal implements Organism, Movable, Eatable {
+    private static long serialUID = 1L;
 
+    @Builder.Default
+    private final long UID = serialUID++;
+
+    @JsonIgnore
+    private Cell cell;
+
+    private Limits limits;
+
+    private String icon;
+    private boolean isAlive = true;
+    private int weight;
+    private int health;
+    private int age;
+
+    public void play() {
+        System.out.println("Animal play");
+        // TODO: add implementation
+    }
+
+    public void eat() {
+
+    }
+
+    public void findFood() {
+
+    }
+
+    public void move() {
+
+    }
 }
--- src/main/java/org/example/entities/animals/abstractions/Herbivore/Herbivore.java
+++ src/main/java/org/example/entities/animals/abstractions/Herbivore/Herbivore.java
@@ -1,7 +1,11 @@
 package org.example.entities.animals.abstractions.Herbivore;
 
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
 import org.example.entities.animals.abstractions.Animal;
 
+@SuperBuilder
+@NoArgsConstructor
 public abstract class Herbivore extends Animal {
 
 }
