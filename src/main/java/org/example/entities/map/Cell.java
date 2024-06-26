bad22a5bfa10db528f8de4e34156787733c3124e
kntsv <kuntsevych2020ki12@student.karazin.ua>
2024-06-26 09:46:36 +0300

    feat: realize code in Cell and GameField classes

--- src/main/java/org/example/entities/map/Cell.java
+++ src/main/java/org/example/entities/map/Cell.java
@@ -1,4 +1,27 @@
 package org.example.entities.map;
 
+import lombok.Builder;
+import lombok.Getter;
+import org.example.abstraction.interfaces.GameObject;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+@Builder
+@Getter
 public class Cell {
+    private static long serialUID = 1l;
+
+    @Builder.Default
+    private final long UID = serialUID++;
+
+    private final Map<Class<? extends GameObject>, Set<GameObject>> residents;
+
+    private final List<Cell> nextCells = new ArrayList<>();
+
+    public void setNext(Cell cell) {
+        nextCells.add(cell);
+    }
 }
--- src/main/java/org/example/entities/map/GameField.java
+++ src/main/java/org/example/entities/map/GameField.java
@@ -1,4 +1,16 @@
 package org.example.entities.map;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.example.abstraction.annotations.Config;
+
+@Config(fileName = "")
+@Getter
+@NoArgsConstructor
 public class GameField {
+
+    private int width;
+    private int height;
+
+    private Cell[][] cells;
 }
