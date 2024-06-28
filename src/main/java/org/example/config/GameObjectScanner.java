3fba9edd71feed51d884be5987863b0a6d917376
kntsv <kuntsevych2020ki12@student.karazin.ua>
2024-06-28 09:43:31 +0300

    feat: implement code logic for GameObjectScanner and PrototypeLoader
    These classes realize scanning files with @Config and @GameObjectEntity annotations and collect them to load their prototypes

--- src/main/java/org/example/config/GameObjectScanner.java
+++ src/main/java/org/example/config/GameObjectScanner.java
@@ -1,4 +1,33 @@
 package org.example.config;
 
+import org.example.abstraction.annotations.Config;
+import org.example.abstraction.annotations.GameObjectEntity;
+import org.example.abstraction.interfaces.GameObject;
+import org.reflections.Reflections;
+
+import java.util.Set;
+import java.util.stream.Collectors;
+
 public class GameObjectScanner {
+    private static GameObjectScanner instance;
+    private final Reflections reflections = new Reflections("org/example");
+
+    private GameObjectScanner() {
+
+    }
+
+    public static GameObjectScanner getInstance() {
+        if (instance == null) {
+            instance = new GameObjectScanner();
+        }
+        return instance;
+    }
+
+    public Set<Class<? extends GameObject>> getAllGameObjectClasses() {
+        return reflections.getSubTypesOf(GameObject.class)
+                .stream()
+                .filter(c -> c.isAnnotationPresent(GameObjectEntity.class))
+                .filter(c -> c.isAnnotationPresent(Config.class))
+                .collect(Collectors.toSet());
+    }
 }
--- src/main/java/org/example/config/PrototypeLoader.java
+++ src/main/java/org/example/config/PrototypeLoader.java
@@ -1,4 +1,54 @@
 package org.example.config;
 
+import com.fasterxml.jackson.core.exc.StreamReadException;
+import com.fasterxml.jackson.databind.DatabindException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.yaml.YAMLMapper;
+import org.example.abstraction.annotations.Config;
+import org.example.abstraction.interfaces.GameObject;
+import org.example.exceptions.InitGameException;
+import org.jetbrains.annotations.NotNull;
+
+import java.io.IOException;
+import java.net.URL;
+
 public class PrototypeLoader {
+    private static PrototypeLoader instance;
+    private ObjectMapper objectMapper = new YAMLMapper();
+
+    private PrototypeLoader() {
+
+    }
+
+    public static PrototypeLoader getInstance() {
+        if(instance == null) {
+            instance = new PrototypeLoader();
+        }
+        return instance;
+    }
+
+    public <T> T loadPrototype(@NotNull Class<T> type) {
+        if (!type.isAnnotationPresent(Config.class)) {
+            throw new IllegalArgumentException(String.format("Prototype class %s must have @Config annotation", type ));
+        }
+        return loadObject(getConfigFilePath(type), type);
+    }
+
+    private <T> T loadObject(@NotNull URL configFilePath, Class<T> type) {
+        T gameObject;
+
+        try {
+            gameObject = objectMapper.readValue(configFilePath, type);
+        } catch (IOException e) {
+            String message = String.format("Can't find config file %s for class %s", configFilePath.getFile(), type);
+            throw new InitGameException(message, e);
+        }
+
+        return gameObject;
+    }
+
+    private URL getConfigFilePath(Class<?> type) {
+        Config config = type.getAnnotation(Config.class);
+        return type.getClassLoader().getResource(config.fileName());
+    }
 }
