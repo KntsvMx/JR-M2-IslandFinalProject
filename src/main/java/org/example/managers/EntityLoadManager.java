bf03cab5c087d16f1a19380a47bea2969827ee0f
kntsv <kuntsevych2020ki12@student.karazin.ua>
2024-06-29 12:13:39 +0300

    feat: add methods for load prototypes' classes
    These methods implement functionality for getting prototypes and store entities classes by factory and store GameField in global field in other class

--- src/main/java/org/example/managers/EntityLoadManager.java
+++ src/main/java/org/example/managers/EntityLoadManager.java
@@ -2,6 +2,7 @@ package org.example.managers;
 
 import org.example.config.GameObjectScanner;
 import org.example.config.PrototypeLoader;
+import org.example.entities.map.GameField;
 import org.example.factory.OrganismFactory;
 
 public class EntityLoadManager {
@@ -21,6 +22,19 @@ public class EntityLoadManager {
             instance = new EntityLoadManager();
         }
         return instance;
+    }
+
+    public void loadPrototypes() {
+        gameObjectScanner
+                .getAllGameObjectClasses()
+                .stream()
+                .map(prototypeLoader::loadPrototype)
+                .forEach(organismFactory::registerPrototype);
+    }
+
+    public GameField loadMapPrototype() {
+        Class<? extends GameField> gameFieldClass = gameObjectScanner.getGameFieldClass();
+        return prototypeLoader.loadPrototype(gameFieldClass);
     }
 
 }
