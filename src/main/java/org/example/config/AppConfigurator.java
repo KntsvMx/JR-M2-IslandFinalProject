548e9d9eed7caac354a54756f48a29e255c6f7c1
kntsv <kuntsevych2020ki12@student.karazin.ua>
2024-07-05 10:17:19 +0300

    feat(WIP): add configure method witch help to make configuration for app

--- src/main/java/org/example/config/AppConfigurator.java
+++ src/main/java/org/example/config/AppConfigurator.java
@@ -1,4 +1,35 @@
 package org.example.config;
 
+import org.example.entities.map.GameField;
+import org.example.managers.EntityBehaviorManager;
+import org.example.managers.EntityLoadManager;
+import org.example.managers.IslandManager;
+
+
 public class AppConfigurator {
+    private static AppConfigurator instance;
+    private final EntityLoadManager loadManager;
+    private final EntityBehaviorManager behaviorManager;
+    private final IslandManager islandManager;
+    private GameField gameField;
+
+    private AppConfigurator() {
+        loadManager = EntityLoadManager.getInstant();
+        behaviorManager = EntityBehaviorManager.getInstance();
+        islandManager = IslandManager.getInstance();
+    }
+
+    public static AppConfigurator getInstance() {
+        if (instance == null) {
+            instance = new AppConfigurator();
+        }
+        return instance;
+    }
+
+    public void configure() {
+        loadManager.loadPrototypes();
+        gameField = loadManager.loadMapPrototype();
+        islandManager.init(gameField);
+        behaviorManager.init();
+    }
 }
