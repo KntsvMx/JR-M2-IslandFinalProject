df3ae867a75550c7549aa353c6fc0d91c2480edc
kntsv <kuntsevych2020ki12@student.karazin.ua>
2024-07-17 09:47:10 +0300

    feat: realize code implementation to work with find food and eat methods in Herbivore
    these changes realize finding classes for eating and eat after all
    
    Took 4 minutes

--- src/main/java/org/example/entities/animals/abstractions/Herbivore/Herbivore.java
+++ src/main/java/org/example/entities/animals/abstractions/Herbivore/Herbivore.java
@@ -2,20 +2,61 @@ package org.example.entities.animals.abstractions.Herbivore;
 
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
+import org.example.abstraction.interfaces.GameObject;
+import org.example.abstraction.interfaces.GameObject;
 import org.example.entities.animals.abstractions.Animal;
 import org.example.entities.interfaces.Eatable;
+
+import java.util.*;
 
 @SuperBuilder
 @NoArgsConstructor
 public abstract class Herbivore extends Animal implements Eatable {
-
     @Override
-    public void eat() {
-
+    public void play() {
+        super.play();
+        findFood();
     }
 
     @Override
-    public void findFood() {
+    public void eat(Optional<Map.Entry<Class<? extends GameObject>, Integer>> matchingTarget, List<GameObject> gameObjects) {
+        if (matchingTarget.isEmpty()) {
+            throw new IllegalArgumentException("Matching target is null");
+        }
 
+        Random random = new Random();
+        Map.Entry<Class<? extends GameObject>, Integer> target = matchingTarget.get();
+//        Class<? extends GameObject> targetClass = target.getKey();
+        Integer targetValue = target.getValue();
+        Animal gameObject = (Animal) gameObjects.stream().findAny().get();
+        int weight = getWeight();
+
+        if (random.nextInt(100) >= targetValue) {
+            weight = weight + gameObject.getWeight();
+            gameObject.setAlive(false);
+            this.setWeight(weight);
+            gameObject.removeGameObjectFromCell();
+        }
+    }
+
+//    TODO: Отрефакторить будущее дублирование кода findFood() в Herbivore and Predator
+
+    @Override
+    public void findFood() {
+        Map<Class<? extends GameObject>, List<GameObject>> residents = getCell().getResidents();
+        Map<Class<? extends GameObject>, Integer> targets = getTarget().getTargetMatrix();
+        Optional<Map.Entry<Class<? extends GameObject>, Integer>> matchingTarget = Optional.empty();
+        List<GameObject> gameObjects = new ArrayList<>();
+
+        for (Map.Entry<Class<? extends GameObject>, List<GameObject>> resident : residents.entrySet()) {
+            matchingTarget = targets.entrySet()
+                    .stream()
+                    .filter(entry -> entry.getKey().equals(resident.getKey()))
+                    .findFirst();
+            gameObjects = resident.getValue();
+
+        }
+
+        eat(matchingTarget, gameObjects);
     }
 }
