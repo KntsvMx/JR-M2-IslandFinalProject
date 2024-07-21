1f8d2d927fc352198b0c90a65f2743d25832d108
kntsv <kuntsevych2020ki12@student.karazin.ua>
2024-07-21 12:30:32 +0300

    refactor: refactor CellGenerations' methods
    refactor methods for better understand
    
    Took 23 minutes

--- src/main/java/org/example/generators/CellGenerator.java
+++ src/main/java/org/example/generators/CellGenerator.java
@@ -10,6 +10,7 @@ import java.util.Map;
 public class CellGenerator {
     private static CellGenerator instance;
     private final ResidenceGenerator residenceGenerator;
+
     private CellGenerator() {
         residenceGenerator = ResidenceGenerator.getInstance();
 
@@ -23,41 +24,52 @@ public class CellGenerator {
     }
 
     public Cell[][] generate(GameField gameField) {
-        Map<Class<? extends GameObject>, List<GameObject>> residents;
         int width = gameField.getWidth();
         int height = gameField.getHeight();
-
         Cell[][] cells = new Cell[width][height];
 
-        for (int i = 0; i < width; i++) {
-            for (int j = 0; j < height; j++) {
-                residents = residenceGenerator.generateNewResidence();
-                cells[i][j] = Cell.builder().residents(residents).build();
-            }
-        }
-
+        fillCells(width, height, cells);
         initializeNeighbors(cells, width, height);
 
         return cells;
     }
 
+    private void fillCells(int width, int height, Cell[][] cells) {
+        for (int i = 0; i < width; i++) {
+            for (int j = 0; j < height; j++) {
+                generateResidentsAndPutThemIntoCell(j, cells[i]);
+            }
+        }
+    }
+
+    private void generateResidentsAndPutThemIntoCell(int j, Cell[] cells) {
+        Map<Class<? extends GameObject>, List<GameObject>> residents = residenceGenerator.generateNewResidence();
+        cells[j] = Cell.builder().residents(residents).build();
+    }
+
     private void initializeNeighbors(Cell[][] cells, int width, int height) {
-        int[] rowOffSets = {-1, 1, 0, 0};
-        int[] colOffSets = {0, 0, -1, 1};
+        int[][] directions = {
+                {-1, 0},
+                {1, 0},
+                {0, -1},
+                {0, 1}
+        };
 
         for (int i = 0; i < width; i++) {
             for (int j = 0; j < height; j++) {
                 Cell current = cells[i][j];
+                addNeighbors(cells, width, height, directions, i, j, current);
+            }
+        }
+    }
 
-                for (int k = 0; k < 4; k++) {
-                    int neighborRow = i + rowOffSets[k];
-                    int neighborCol = j + colOffSets[k];
+    private void addNeighbors(Cell[][] cells, int width, int height, int[][] directions, int i, int j, Cell current) {
+        for (int[] direction: directions) {
+            int neighborRow = i + direction[0];
+            int neighborCol = j + direction[1];
 
-                    if (isValidCell(neighborRow, neighborCol, width, height)) {
-                        current.setNext(cells[neighborRow][neighborCol]);
-                    }
-                }
-
+            if (isValidCell(neighborRow, neighborCol, width, height)) {
+                current.setNext(cells[neighborRow][neighborCol]);
             }
         }
     }
