55057a3cf8fe95f69e3e42507d2c1fdc82a8a186
Maksym Kuntsevych <kuntsevychmaksymwork@gmail.com>
2024-10-07 09:54:10 +0300

    feat: add setCell method
    This method helps to set cells for initiation state or due gameObject movement on the map
    
    Took 1 minute

--- src/main/java/org/example/abstraction/interfaces/GameObject.java
+++ src/main/java/org/example/abstraction/interfaces/GameObject.java
@@ -1,7 +1,9 @@
 package org.example.abstraction.interfaces;
 
 import org.example.entities.interfaces.Reproducible;
+import org.example.entities.map.Cell;
 
 public interface GameObject extends Reproducible {
     void play();
+    void setCell(Cell cell);
 }
