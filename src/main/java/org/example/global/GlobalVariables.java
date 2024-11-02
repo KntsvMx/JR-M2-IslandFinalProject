ebf26823ef8b84d0c743750f4dd5b5066365e031
Maksym Kuntsevych <kuntsevychmaksymwork@gmail.com>
2024-11-02 09:31:56 +0200

    feat: add GlobalVariables as lock controller
    It helps to lock cells during their usage
    
    Took 8 minutes

---
+++ src/main/java/org/example/global/GlobalVariables.java
@@ -0,0 +1,9 @@
+package org.example.global;
+
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class GlobalVariables {
+    public static final ReentrantLock lock = new ReentrantLock();
+    public static final ThreadLocalRandom random = ThreadLocalRandom.current();
+}
