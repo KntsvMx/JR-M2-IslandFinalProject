34add1e769de5241da39d7e9cbf7c4a0801e614d
Maksym Kuntsevych <kuntsevychmaksymwork@gmail.com>
2024-11-25 09:53:27 +0200

    feat: implement CellManager's methods in the move method of MoveBehavior
    Took 26 minutes

--- src/main/java/org/example/behaviour/generalBehaviorStatements/MoveBehavior.java
+++ src/main/java/org/example/behaviour/generalBehaviorStatements/MoveBehavior.java
@@ -2,16 +2,24 @@ package org.example.behaviour.generalBehaviorStatements;
 
 import org.example.entities.animals.abstractions.Animal;
 import org.example.entities.map.InteractableCell;
+import org.example.managers.CellManager;
 
 public class MoveBehavior {
+    private final CellManager cellManager;
+
+    public MoveBehavior() {
+        cellManager = CellManager.getInstance();
+    }
 
     public void move(Animal animal) {
         synchronized (animal) {
             InteractableCell nextCell;
+            InteractableCell currentCell = animal.getCell();
 
             if (animal.getHealth() >= 60) {
                 nextCell = animal.getCell().getRandomCellFromClosest();
-                animal.move(nextCell);
+                cellManager.addGameObject(nextCell, animal);
+                cellManager.removeGameObject(currentCell, animal);
                 animal.changeHealthAfterAction();
             }
         }
