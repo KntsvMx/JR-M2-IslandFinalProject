e2bcadd05be165d2c615d3858c025f2e3bd26da6
Maksym Kuntsevych <kuntsevychmaksymwork@gmail.com>
2024-11-28 19:02:22 +0200

    feat: delete by deadlock issue
    Took 30 minutes

--- src/main/java/org/example/global/GlobalVariables.java
+++
@@ -1,9 +0,0 @@
-package org.example.global;
-
-import java.util.concurrent.ThreadLocalRandom;
-import java.util.concurrent.locks.ReentrantLock;
-
-public class GlobalVariables {
-    public static final ReentrantLock lock = new ReentrantLock();
-    public static final ThreadLocalRandom random = ThreadLocalRandom.current();
-}
