f4d2ac3a674f0e707b281e3dc8f2f2db2a69b1df
Maksym Kuntsevych <kuntsevychmaksymwork@gmail.com>
2024-12-02 09:17:50 +0200

    fix: fix logical bug at move method and add check for available space in next cell
    Took 35 minutes

--- src/main/java/org/example/behaviour/generalBehaviorStatements/MoveBehavior.java
+++ src/main/java/org/example/behaviour/generalBehaviorStatements/MoveBehavior.java
@@ -5,6 +5,7 @@ import org.example.entities.map.Cell;
 import org.example.entities.map.InteractableCell;
 import org.example.managers.CellManager;
 
+import java.util.List;
 import java.util.concurrent.locks.ReentrantLock;
 
 public class MoveBehavior {
@@ -19,16 +20,22 @@ public class MoveBehavior {
         lock.lock();
         try {
             InteractableCell currentCell = animal.getCell();
+            Cell randomCellFromClosest = cellManager.getRandomCellFromClosest(currentCell);
+            int maxAmount = animal.getLimits().getMaxAmount();
 
-//          TODO: 2024-11-28(added) need to add check for available space in cell
-            if (animal.getHealth() >= 60) {
-                Cell randomCellFromClosest = cellManager.getRandomCellFromClosest(currentCell);
+            if (animal.getHealth() >= 60 && availableSpaceForSpecie(randomCellFromClosest, maxAmount)) {
                 animal.move(randomCellFromClosest);
                 animal.changeHealthAfterAction();
             }
         } finally {
             lock.unlock();
         }
+    }
+
+//  TODO: 2024-12-01(added) need to refactor code to avoid code duplication in MoveBehavior and ReproduceBehavior classes
+    private boolean availableSpaceForSpecie(InteractableCell sameSpecie, Integer maxAmount) {
+        int amountOfSpecie = sameSpecie.getResidents().getOrDefault(sameSpecie.getClass(), List.of()).size();
+        return amountOfSpecie < maxAmount;
     }
 }
 
