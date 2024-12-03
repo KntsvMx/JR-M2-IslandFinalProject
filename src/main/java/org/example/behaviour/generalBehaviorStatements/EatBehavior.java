7591e7b688b137e7bfc9033d560351730cc0c3e7
Maksym Kuntsevych <kuntsevychmaksymwork@gmail.com>
2024-12-03 19:34:57 +0200

    feat: add realization of multithreading and observer notification
    Took 27 minutes

--- src/main/java/org/example/behaviour/generalBehaviorStatements/EatBehavior.java
+++ src/main/java/org/example/behaviour/generalBehaviorStatements/EatBehavior.java
@@ -6,33 +6,64 @@ import org.example.entities.interfaces.Eatable;
 import org.example.entities.map.InteractableCell;
 import org.example.entities.plants.Plant;
 import org.example.managers.CellManager;
+import org.example.statistic.interfaces.Observer;
+import org.example.statistic.interfaces.Subject;
 
 import javax.annotation.Nullable;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.Random;
+import java.util.concurrent.locks.ReentrantLock;
 
-public class EatBehavior {
+public class EatBehavior implements Subject {
+    private List<Observer> observers = new ArrayList<>();
     private final CellManager cellManager;
+    private final ReentrantLock lock = new ReentrantLock();
 
     public EatBehavior() {
         cellManager = CellManager.getInstance();
     }
 
     public void findFood(Animal animal) {
-        Map<Class<? extends GameObject>, List<GameObject>> residents = animal.getCell().getResidents();
-        Map<Class<? extends GameObject>, Integer> targets = animal.getTarget().getTargetMatrix();
 
-//    TODO: Probably available to change to lambda expression
-
-        for (Map.Entry<Class<? extends GameObject>, Integer> target : targets.entrySet()) {
-            List<GameObject> potentialFood = residents.get(target.getKey());
-            Integer targetValue = target.getValue();
-            if (potentialFood != null && !potentialFood.isEmpty()) {
-                eat(animal, potentialFood, targetValue);
-                return;
-            }
+        if (isHungry(animal)) {
+            return;
         }
+
+        lock.lock();
+        try {
+            Map<Class<? extends GameObject>, List<GameObject>> residents = animal.getCell().getResidents();
+            Map<Class<? extends GameObject>, Integer> targets = animal.getTarget().getTargetMatrix();
+
+//    TODO: Probably available to change to lambda expression below
+//            targets.entrySet().stream()
+//                    .filter(target -> {
+//                        List<GameObject> potentialFood = residents.get(target.getKey());
+//                        return potentialFood != null && !potentialFood.isEmpty();
+//                    })
+//                    .findFirst()
+//                    .ifPresent(target -> {
+//                        List<GameObject> potentialFood = residents.get(target.getKey());
+//                        Integer targetValue = target.getValue();
+//                        eat(animal, potentialFood, targetValue);
+//                    });
+
+            for (Map.Entry<Class<? extends GameObject>, Integer> target : targets.entrySet()) {
+                List<GameObject> potentialFood = residents.get(target.getKey());
+                Integer targetValue = target.getValue();
+                if (potentialFood != null && !potentialFood.isEmpty()) {
+                    eat(animal, potentialFood, targetValue);
+                    return;
+                }
+            }
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    private boolean isHungry(Animal animal) {
+        return animal.getWeight() < animal.getLimits().getMaxWeight();
     }
 
     private void eat(Animal animal, List<GameObject> potentialFood, Integer targetValue) {
@@ -41,23 +72,27 @@ public class EatBehavior {
         int currentWeight = animal.getWeight();
         int SUBTRACTING_WEIGHT = - 10;
         InteractableCell interactableCell = animal.getCell();
+        interactableCell.getLock().lock();
+        try {
+            GameObject gameObject = findAnyFirstPotentialFood(potentialFood);
 
-        GameObject gameObject = findAnyFirstPotentialFood(potentialFood);
+            if (gameObject == null) {
+                throw new IllegalArgumentException("Game object not found");
+            }
 
-        if (gameObject == null) {
-            throw new IllegalArgumentException("Game object not found");
-        }
+            if (randomIndex >= targetValue || targetValue == 100) {
+                Eatable eatenObject = (Eatable) gameObject;
+                eatenObject.beEaten();
 
-        if (randomIndex >= targetValue || targetValue == 100) {
-            Eatable eatenObject = (Eatable) gameObject;
-            eatenObject.beEaten();
-
-            animal.setWeight(calculateNutritionalValue(gameObject));
-//            TODO: observe changing statement if object has eaten
-            cellManager.removeGameObject(interactableCell, (GameObject) eatenObject);
-        } else {
-            animal.changeHealthAfterAction();
-            animal.setWeight(currentWeight - SUBTRACTING_WEIGHT);
+                animal.setWeight(calculateNutritionalValue(gameObject));
+                observers.forEach(Observer::updateKilled);
+                cellManager.removeGameObject(interactableCell, (GameObject) eatenObject);
+            } else {
+                animal.changeHealthAfterAction();
+                animal.setWeight(currentWeight - SUBTRACTING_WEIGHT);
+            }
+        } finally {
+            interactableCell.getLock().unlock();
         }
     }
 
@@ -76,5 +111,19 @@ public class EatBehavior {
             return ((Plant) gameObject).getWeight();
         }
         return 0;
+    }
+
+    @Override
+    public void addObserver(Observer observer) {
+        observers.add(observer);
+    }
+
+    @Override
+    public void removeObserver(Observer observer) {
+        observers.remove(observer);
+    }
+
+    @Override
+    public void notifyObservers() {
     }
 }
