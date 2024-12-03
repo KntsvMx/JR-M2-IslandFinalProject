d01cbc0de7e8baeffe59a8660f394623a5c024b5
Maksym Kuntsevych <kuntsevychmaksymwork@gmail.com>
2024-12-03 09:34:53 +0200

    feat: implement code into methods and prepare statistic overview
    by using Observer's pattern will check last update on Island
    
    Took 11 minutes

--- src/main/java/org/example/statistic/StatisticMonitor.java
+++ src/main/java/org/example/statistic/StatisticMonitor.java
@@ -1,14 +1,13 @@
 package org.example.statistic;
 
-import lombok.Getter;
 import lombok.Setter;
 import org.example.statistic.interfaces.Observer;
 
 @Setter
-@Getter
 
-//    TODO: realize observer pattern here
 public class StatisticMonitor implements Observer {
+    private long timeOfLivingIsland = 0;
+    private int cycleCount = 0;
     private int organisms = 0;
     private int aliveOrganisms = 0;
     private int killedOrganisms = 0;
@@ -16,39 +15,64 @@ public class StatisticMonitor implements Observer {
     private int deathOrganisms = 0;
     private int plantAlive = 0;
 
-
-//    TODO: add realization to printing statistic in console
     public void printStatistics() {
-
+        StringBuilder sb = new StringBuilder();
+        countTimeOfLivingIsland();
+        sb.append(String.format("Current cycle: %d%n", cycleCount));
+        sb.append("Statistics:%n");
+        sb.append(String.format("Total organisms: %d%n", organisms));
+        sb.append(String.format("Alive organisms: %d%n", aliveOrganisms));
+        sb.append(String.format("Killed organisms: %d%n", killedOrganisms));
+        sb.append(String.format("Born organisms: %d%n", bornOrganisms));
+        sb.append(String.format("Death organisms: %d%n", deathOrganisms));
+        sb.append(String.format("Alive plants: %d%n", plantAlive));
+        System.out.print(sb.toString());
     }
 
-//    TODO: rename soon
-    public void countTimeOfLivingIsland() {
+    private void countTimeOfLivingIsland() {
+        long minutes = timeOfLivingIsland / 60000;
+        long seconds = (timeOfLivingIsland / 1000) % 60;
+        System.out.printf("Time of living island: %d minutes %d seconds%n", minutes, seconds);
+    }
 
+    @Override
+    public void updateTime(long startTime, long endTime) {
+        timeOfLivingIsland = endTime - startTime;
     }
 
     @Override
     public void updateKilled() {
-
+        killedOrganisms++;
     }
 
     @Override
     public void updateAlive() {
-
+        aliveOrganisms++;
     }
 
     @Override
     public void updateBorn() {
-
+        bornOrganisms++;
     }
 
     @Override
     public void updateDeath() {
-
+        deathOrganisms++;
     }
 
     @Override
     public void updatePlant() {
+        plantAlive++;
+    }
 
+
+    @Override
+    public void updateCycle() {
+        cycleCount++;
+    }
+
+    @Override
+    public void updateTotalOrganisms(int totalOrganisms) {
+        setOrganisms(totalOrganisms);
     }
 }
--- src/main/java/org/example/statistic/interfaces/Observer.java
+++ src/main/java/org/example/statistic/interfaces/Observer.java
@@ -1,9 +1,12 @@
 package org.example.statistic.interfaces;
 
 public interface Observer {
+    void updateTime(long startTime, long endTime);
     void updateKilled();
     void updateAlive();
     void updateBorn();
     void updateDeath();
     void updatePlant();
+    void updateCycle();
+    void updateTotalOrganisms(int totalOrganisms);
 }
