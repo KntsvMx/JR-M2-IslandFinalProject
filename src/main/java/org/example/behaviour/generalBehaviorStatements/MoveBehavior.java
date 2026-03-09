f6b089e52c956a0dfa19d585fa04307013852a06
Maksym Kuntsevych <kuntsevychmaksymwork@gmail.com>
2026-03-09 11:01:35 +0200

    refactor: simplify movement logic in MoveBehavior and enhance locking mechanism

--- src/main/java/org/example/behaviour/generalBehaviorStatements/MoveBehavior.java
+++ src/main/java/org/example/behaviour/generalBehaviorStatements/MoveBehavior.java
@@ -1,19 +1,15 @@
 package org.example.behaviour.generalBehaviorStatements;
 
 import org.example.entities.animals.abstractions.Animal;
-import org.example.entities.map.Cell;
 import org.example.entities.map.InteractableCell;
-import org.example.managers.CellManager;
 import org.example.utils.SpaceUtil;
 
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.locks.ReentrantLock;
 
+//TODO: fix bug with move when target cell isn't locked which can cause of ConcurrentModificationException
+
 public class MoveBehavior {
-
-    public MoveBehavior() {
-
-    }
 
     public void move(Animal animal, InteractableCell fromCell, InteractableCell toCell) {
         ReentrantLock targetLock = toCell.getLock();
@@ -30,19 +26,24 @@ public class MoveBehavior {
         if (gotLock) {
             try {
                 boolean hasSpace = SpaceUtil.availableSpaceForSpecie(toCell, animal);
+//                TODO: optimize this check by change method from.getResidents() to CellManager
                 boolean isHere = fromCell.getResidents().get(animal.getClass()).contains(animal);
-
-                if (hasSpace && isHere) {
-                    fromCell.removeGameObjectFromResidents(animal);
-                    toCell.addGameObjectToResidents(animal.getClass(), animal);
-
-                    animal.setCell(toCell);
-
-                    animal.changeHealthAfterMove();
-                }
+                executeMovementIfValid(animal, fromCell, toCell, hasSpace, isHere);
             } finally {
                 targetLock.unlock();
             }
+        }
+    }
+
+    private static void executeMovementIfValid(Animal animal, InteractableCell fromCell, InteractableCell toCell, boolean hasSpace, boolean isHere) {
+        if (hasSpace && isHere) {
+            fromCell.removeGameObjectFromResidents(animal);
+            toCell.addGameObjectToResidents(animal.getClass(), animal);
+
+//                  TODO: optimize this by change method animal.setCell() to CellManager
+            animal.setCell(toCell);
+
+            animal.changeHealthAfterMove();
         }
     }
 }
