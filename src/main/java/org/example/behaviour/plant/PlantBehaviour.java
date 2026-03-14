6189e7f80cd849e5f109c95e30628dc7566f470d
Maksym Kuntsevych <kuntsevychmaksymwork@gmail.com>
2026-03-14 09:01:01 +0200

    feat: integrate DeathManager for plant health management in grow method

--- src/main/java/org/example/behaviour/plant/PlantBehaviour.java
+++ src/main/java/org/example/behaviour/plant/PlantBehaviour.java
@@ -4,6 +4,7 @@ import org.example.behaviour.generalBehaviorStatements.ReproduceBehavior;
 import org.example.entities.map.InteractableCell;
 import org.example.entities.plants.Grass;
 import org.example.entities.plants.Plant;
+import org.example.managers.DeathManager;
 import org.example.statistic.AbstractSubject;
 
 import java.util.function.Consumer;
@@ -19,6 +20,9 @@ public class PlantBehaviour extends AbstractSubject {
     public void grow(InteractableCell cell) {
         processPlants(cell, plant -> {
             reproduceBehavior.reproduce(plant, cell);
+            if (plant.decreaseHealthOverTime()) {
+                DeathManager.getInstant().registerDeath(plant, cell);
+            }
         });
     }
 
